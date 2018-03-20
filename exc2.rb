f0444d33d013baa67fc28018431ed126c195e0dc
Marat200118 <marat2001@inbox.lv>
2018-03-20 15:58:03 +0200

    edit_exc2

--- exc2.rb
+++ exc2.rb
@@ -1,1 +1,15 @@
-puts "hello, world"
+  my_name = 'Peter'
+  my_age = 16
+  my_height = 170
+  my_weight = 55
+  my_eyes = 'grey'
+  my_teeth = 'White'
+  my_hair = 'Brown'
+
+  puts "Let's talk about #{my_name}."
+  puts "He's #{my_height} cm tall."
+  puts "He's #{my_weight} kg heavy."
+  puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+  puts "He's teeth is ussualy #{my_teeth}."
+
+  puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
