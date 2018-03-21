6cb32cb3ec8cd5eea2cedf59c167bc38f07e1f8b
Marat200118 <marat2001@inbox.lv>
2018-03-21 17:27:01 +0200

    learning ruby

---
+++ exc6.rb
@@ -0,0 +1,11 @@
+print "Give me a money: "
+number = gets.chomp.to_f
+
+money = number * 0.1
+puts "A cashback is #{money}."
+
+print "Give me a another money: "
+number = gets.chomp.to_f
+
+money = number * 0.2
+puts "A another cashback is #{money}."
